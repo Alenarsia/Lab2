3900a695260279465b6499a50d7cafa29bc96acc
Александр Заварницын <bocctabiiiuu10@gmail.com>
2020-11-30 12:19:15 +0300

    Интерфейс

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,52 +6,77 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Приветственное сообщение.");
             double a;
             double b;
             double total;
+            bool check = false;
             char oper;
-            Console.WriteLine("Введите первое число:");
-            a = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Введите оператор:");
-            oper = Convert.ToChar(Console.ReadLine());
-
-            Console.WriteLine("Введите второе число:");
-            b = Convert.ToDouble(Console.ReadLine());
-            if (oper == '+')
+            while (check == false)
             {
-                total = a + b;
-                Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
-                Console.WriteLine("Измениние 1.");
-                Console.WriteLine("Измениние 2.");
-            }
+                Console.WriteLine("Введите первое число:");
+                a = Convert.ToDouble(Console.ReadLine());
 
-            else if (oper == '-')
-            {
-                total = a - b;
-                Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
-                Console.WriteLine("Измениние 1.");
-                Console.WriteLine("Измениние 2.");
-            }
+                Console.WriteLine("Введите оператор:");
+                oper = Convert.ToChar(Console.ReadLine());
 
-            else if (oper == '*')
-            {
-                total = a * b;
-                Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
-                Console.WriteLine("Измениние 1.");
-                Console.WriteLine("Измениние 2.");
-            }
+                Console.WriteLine("Введите второе число:");
+                b = Convert.ToDouble(Console.ReadLine());
+                if (oper == '+')
+                {
+                    total = a + b;
+                    Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+                    Console.WriteLine("Измениние 1.");
+                    Console.WriteLine("Измениние 2.");
+                    Console.WriteLine("Продолжить?Y/N.");
+                    if (Console.ReadKey().Key == ConsoleKey.N)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
 
-            else if (oper == '/')
-            {
-                total = a / b;
-                Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
-                Console.WriteLine("Измениние 1.");
-                Console.WriteLine("Измениние 2.");
-            }
-            else
-            {
-                Console.WriteLine("Неизвестный оператор.");
+                else if (oper == '-')
+                {
+                    total = a - b;
+                    Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+                    Console.WriteLine("Измениние 1.");
+                    Console.WriteLine("Измениние 2.");
+                    Console.WriteLine("Продолжить?Y/N.");
+                    if (Console.ReadKey().Key == ConsoleKey.N)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+
+                else if (oper == '*')
+                {
+                    total = a * b;
+                    Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+                    Console.WriteLine("Измениние 1.");
+                    Console.WriteLine("Измениние 2.");
+                    Console.WriteLine("Продолжить?Y/N.");
+                    if (Console.ReadKey().Key == ConsoleKey.N)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+
+                else if (oper == '/')
+                {
+                    total = a / b;
+                    Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+                    Console.WriteLine("Измениние 1.");
+                    Console.WriteLine("Измениние 2.");
+                    Console.WriteLine("Продолжить?Y/N.");
+                    if (Console.ReadKey().Key == ConsoleKey.N)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестный оператор.");
+                }
             }
         }
     }
